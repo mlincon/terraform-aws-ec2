365cec19343302067256a6fe4764614bae9782ef
mlincon <mehdi.lincon@gmail.com>
2020-11-28 12:27:34 +0100

    initial commit

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  region  = var.region
+  profile = var.profile
+
+  // pathexpand converts "~" to /home/user, i.e. $HOME
+  shared_credentials_file = pathexpand(var.credentials_file)
+}
---
+++ vpc.tf
@@ -0,0 +1,6 @@
+// In general, AWS creates a default VPC for each AWS region when the
+// account is created
+
+resource "aws_vpc" "ec2-vpc" {
+
+}
